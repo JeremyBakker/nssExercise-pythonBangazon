79c0a388a1fca8258d2e562eff899c20425c700b
Jeremy Bakker <jeremybakker@jeremys-mbp.attlocal.net>
2017-04-17 14:46:13 -0500

    finished through exercise four

--- bangazon.py
+++ bangazon.py
@@ -1,3 +1,5 @@
+import random
+
 '''This module creates departments for a fictitious company.
 
     Classes: Department, HR, Admin, Sales
@@ -157,6 +159,84 @@ class Sales(Department):
         '''Prints a message identifying a meeting location for the Human Resources department
         '''
         print("Call 1-800-263-3476. The pass code to access the conference call is #2937.")
+
+class Employee: 
+    '''Class representing an employee'''
+    
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.restaurants = ["Local Taco", "Bobbie's Dairy Dip", "Miel", "Bajo Sexto", 
+        "Caffee Nonna", "Coco's Italian Market"]
+
+    def eat(self, food = False, companions = False):
+        if food and companions == False:
+            restaurant = "the office."
+        else:
+            restaurant = self.restaurants[random.randint(0,5)]
+
+        if companions:
+            companions = list(companions)
+
+            if len(companions) == 1:
+                companions = str(companions)
+                companions_list = companions[2:-2]
+            else:
+                companions = str(companions)
+                companions = companions[1:-1]
+                companions_list = companions.replace("'", "")
+
+        print("{} {} ate {} at {} with {}.".format(self.first_name, self.last_name, food, restaurant, companions_list))
+
+class FullTime():
+    '''Class representing Full-Time employee'''
+
+    def __init__(self):
+        self.hours_per_week = 40
+
+class PartTime():
+    '''Class representing Part-Time employee'''
+
+    def __init__(self):
+        self.hours_per_week = 20
+
+class AccessCard():
+    '''Class representing employee that need Access Card'''
+
+    def __init__(self):
+        self.access_card = True
+
+class AdaAccess():
+    '''Class representing employee that need Access Card'''
+
+    def __init__(self):
+        self.ada_access = False
+
+class HREmployee(Employee, FullTime, AccessCard, AdaAccess):
+    '''Class representing Human Resources Employee'''
+
+    def __init__(self, firstName, lastName):
+        super().__init__(firstName, lastName)
+        FullTime.__init__(self)
+        AccessCard.__init__(self)
+        AdaAccess.__init__(self)
+
+class AdminEmployee(Employee, FullTime, AccessCard, AdaAccess):
+    '''Class representing Human Resources Employee'''
+
+    def __init__(self, firstName, lastName):
+        super().__init__(firstName, lastName)
+        FullTime.__init__(self)
+        AccessCard.__init__(self)
+        AdaAccess.__init__(self)
+
+class SalesEmployee(Employee, FullTime, AccessCard, AdaAccess):
+    '''Class representing Human Resources Employee'''
+
+    def __init__(self, firstName, lastName):
+        super().__init__(firstName, lastName)
+        FullTime.__init__(self)
+        AdaAccess.__init__(self)
 
 hr_department = HR("Human Resources", "Shawn", 2)
 admin_department = Admin("Administration", "Elijah", 4)
